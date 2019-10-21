18f68c8376ef60338d9f1f35ed473958dced6c94
Matt Brown <hi@mattbrown.email>
2019-10-21 14:50:09 -0500

    Initial commit

--- express/server.js
+++ express/server.js
@@ -7,8 +7,9 @@ const bodyParser = require('body-parser');
 
 const router = express.Router();
 router.get('/', (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write('<h1>Hello from Express.js!</h1>');
+  // res.writeHead(200, { 'Content-Type': 'text/html' });
+  // res.write('<h1>Hello from Express.js!</h1>');
+  res.write("hello");
   res.end();
 });
 router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
